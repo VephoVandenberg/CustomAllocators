bd472962c27f7d8acae92524f450a5999ae6edaa
Azamat Tskhovrebov <AzamatTheBest.cs.py@gmail.com>
2022-06-10 17:41:39 +0300

    Update VPMemory.h

--- memoryAllocator3/src/VPMemory.h
+++ memoryAllocator3/src/VPMemory.h
@@ -1,1 +1,16 @@
+#include <unistd.h>
 
+typedef struct header
+{
+    struct header *next;
+    size_t size;
+    char isFree;
+}header_t;
+
+#define HEADER_SIZE sizeof(header_t)
+
+void *VPMalloc(size_t size);
+void VPFree(void *pointer);
+
+header_t *findFreeBlock(size_t size);
+header_t *requestSpace(size_t size);
