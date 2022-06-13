2735714b66a078ab314f80acf4ce48ea445228fe
Azamat Tskhovrebov <AzamatTheBest.cs.py@gmail.com>
2022-06-13 14:51:15 +0300

    Update main.c

--- memoryAllocator3/main.c
+++ memoryAllocator3/main.c
@@ -11,20 +11,26 @@ int main(int argc, char **argv)
 
 void testAllocator()
 {
-    
     int *a = VPMalloc(sizeof(int));
-    printf("Memory address is %ld \n", a);
+    printf("Memory address is %x \n", a);
     *a = 35;
     printf("With value of %ld\n", *a);
     
     int *b = VPMalloc(sizeof(int));
-    printf("Memory address is %ld \n", b);
+    printf("Memory address is %x \n", b);
     *b = 44;
     printf("With value of %ld\n", *b);
 
+    printf("Memory address of a is %x\n", a);
+
     short *c = VPMalloc(sizeof(short));
-    printf("Memory address is %ld \n", c);
+    printf("Memory address is %x \n", c);
     *c = 35;
     printf("With value of %d\n", *c);
+
+    printf("Check if the memory remains the same\n");
+    printf("Memory address of a is %x\n", a);
+    printf("Memory address of b is %x\n", b);
+    
 }
 
