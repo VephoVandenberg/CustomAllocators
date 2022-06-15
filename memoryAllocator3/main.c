0d837bd240668775e956f7fd03242f9c5b901229
Azamat Tskhovrebov <AzamatTheBest.cs.py@gmail.com>
2022-06-15 19:22:45 +0300

    Update main.c

--- memoryAllocator3/main.c
+++ memoryAllocator3/main.c
@@ -5,7 +5,7 @@ void testAllocator();
 
 int main(int argc, char **argv)
 {
-
+    testAllocator();
     return 0;
 }
 
@@ -13,28 +13,34 @@ void testAllocator()
 {
     
     int *a = VPMalloc(sizeof(int));
-    printf("Memory address of 'a' is 0X%x \n", a);
+    printf("Memory address in 'a' is %p \n", a);
     *a = 35;
     printf("With value of %ld\n", *a);
     
     int *b = VPMalloc(sizeof(int));
-    printf("Memory address of 'b' is 0X%x \n", b);
+    printf("Memory address in 'b' is %p\n", b);
     *b = 44;
     printf("With value of %ld\n", *b);
 
 
     short *c = VPMalloc(sizeof(short));
-    printf("Memory address of 'c' is 0X%x \n", c);
+    printf("Memory address in 'c' is %p \n", c);
     *c = 35;
     printf("With value of %d\n\n", *c);
 
-    printf("Now let's free the variable 'b'\n\n");
+    printf("Now let's free the variable 'b'\n");
     VPFree(b);
+    printf("The memory address in 'b' %p\n\n", b);
     
     printf("Check if the memory remains the same\n");
-    printf("Memory address of a is 0X%x\n", a);
-    printf("Memory address of b is 0X%x\n", b);
-    printf("Memory address of c is 0X%x\n\n", c);
+    printf("Memory address of a is %p\n", a);
+    printf("Memory address of b is %p\n", b);
+    printf("Memory address of c is %xpn\n", c);
     
     printList();
+
+    printf("Free 'a' \n");
+    VPFree(a);
+    printList();
 }
+
