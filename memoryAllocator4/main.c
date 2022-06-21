6ae18495bd5b646b739a558cb7a0d0c9abb0b1ba
Azamat Tskhovrebov <AzamatTheBest.cs.py@gmail.com>
2022-06-21 18:00:48 +0300

    Update main.c

--- memoryAllocator4/main.c
+++ memoryAllocator4/main.c
@@ -1,1 +1,54 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#define ui8 unsigned char
+#define ui16 unsigned short
+#define ui32 unsigned int
+#define ui64 unsigned long
+
+#define i8 char
+#define i16 short
+#define i32 int
+#define i64 long
+
+typedef struct header
+{
+    ui8 isFree;
+    ui32 prevLen;
+    ui32 currLen;
+} header_t;
+
+#define HEADER_SIZE sizeof(header_t)
+#defome FREE_LIST_SIZE 64
+
+header_t *start;
+header_t *end;
+
+header_t freeList[FREE_LIST_SIZE];
+
+void *VVMalloc(ui32 size);
+void VVFree();
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
+
+void *VVMalloc(ui32 size)
+{
+    void *header = sbrk(0);
+    void *memBlock = sbrk(size);
+
+    if (memBlock == (void *)(-1))
+    {
+	return NULL;
+    }
+    
+    return (header + 1);
+}
+
+void VVFree()
+{
+    
+}
 
